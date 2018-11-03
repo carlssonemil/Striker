bbebb9f86509159662b2b3103d1cda5f82ba905f
Emil Carlsson <emil-carlsson@live.se>
2018-11-03 12:25:05 +0100

    Add console warnings :zap:

--- src/index.js
+++ src/index.js
@@ -8,13 +8,26 @@ const client = new commando.Client({
   unknownCommandResponse: false
 });
 
-client.registry.registerGroup('strike', 'Strike');
-client.registry.registerDefaults();
-client.registry.registerCommandsIn(__dirname + "/commands");
+client
+  .on('error', console.error)
+  .on('warn', console.warn)
+  .on('debug', console.log)
+  .on('ready', () => {
+    console.log(`Client is ready; logged in as ${ client.user.tag } (${ client.user.id })`);
+    client.user.setActivity('Half Life 3'); // Easter egg 🤭
+  })
+  .on('disconnect', () => { console.warn('Disconnected!'); })
+  .on('reconnecting', () => { console.warn('Reconnecting...'); })
+  .on('commandError', (command, error) => {
+    if (error instanceof commando.FriendlyError) return;
+    console.error(`Error in command ${ command.groupID }:${ command.memberName }`, error);
+  });
 
-client.on('ready', () => {
-  console.log('Logged in!');
-  client.user.setActivity('Half Life 3');
-});
+client.registry
+  .registerGroups([
+    ['strike', 'Strike – Commands handling the strike functionality']
+  ])
+  .registerDefaults()
+  .registerCommandsIn(__dirname + '/commands');
 
 client.login(process.env.BOT_TOKEN);
