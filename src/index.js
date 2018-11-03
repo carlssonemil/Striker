0da506e0465a4fc8e254c761dd0e7d50f1ffdd77
Emil Carlsson <emil-carlsson@live.se>
2018-11-03 11:28:20 +0100

    Add 'ready' function that prints to console when Bot is active :hammer:

--- src/index.js
+++ src/index.js
@@ -12,4 +12,9 @@ client.registry.registerGroup('strike', 'Strike');
 client.registry.registerDefaults();
 client.registry.registerCommandsIn(__dirname + "/commands");
 
+client.on('ready', () => {
+  console.log('Logged in!');
+  client.user.setActivity('Half Life 3');
+});
+
 client.login(process.env.BOT_TOKEN);
